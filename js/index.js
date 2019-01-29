10a0db2bbd2be06c389db9637c16db9603d62c75
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-29 16:35:36 +0100

    refac: Refactor code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-document.querySelector("h1.logo-heading").addEventListener("click", function() {
-	TweenMax.to("#littleImg", 8, { right: 2000 });
-});
-
 // 1. COPY EVENT LISTENER
 document.body.addEventListener("copy", function() {
 	prompt("You copied something! Do you want to share it with me? 😍");
@@ -89,7 +85,6 @@ document.querySelectorAll(".nav-link").forEach(el => {
 });
 
 //PROPAGATION
-
 document.querySelector(".footer p").addEventListener("click", function(event) {
 	const { target } = event;
 	target.style.color = "green";
@@ -99,4 +94,9 @@ document.querySelector(".footer p").addEventListener("click", function(event) {
 document.querySelector("footer").addEventListener("click", function(event) {
 	const { target } = event;
 	target.style.backgroundColor = "yellow";
+});
+
+//STRETCH GOAL ANIMATION
+document.querySelector("h1.logo-heading").addEventListener("click", function() {
+	TweenMax.to("#littleImg", 8, { right: 2000 });
 });
