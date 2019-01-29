774e28882823f8dd6f3837ad55df1831b97dc4a3
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-29 14:39:28 +0100

    feat: Add event listeners to index.js

--- js/index.js
+++ js/index.js
@@ -16,6 +16,26 @@ document.querySelector("h1.logo-heading").addEventListener("mouseover", function
 	}, 2000);
 });
 
+//ANIMATION START EVENT LISTENER
+document.querySelector("h1.logo-heading").addEventListener("animationstart", function() {
+	console.log("bla");
+	document.querySelectorAll(".nav-link")[0].style.color = "blue";
+	document.querySelectorAll(".nav-link")[1].style.color = "yellow";
+	document.querySelectorAll(".nav-link")[2].style.color = "red";
+	document.querySelectorAll(".nav-link")[3].style.color = "green";
+	setTimeout(function() {
+		document.querySelectorAll(".nav-link").forEach(el => {
+			el.style.color = "black";
+		});
+	}, 2000);
+});
+
+//MOUSEDOWN EVENT LISTENER
+document.querySelector("header img").addEventListener("mousedown", function(eventInfo) {
+	const { target } = eventInfo;
+	target.classList.toggle("filterClass");
+});
+
 //KEYDOWN EVENT LISTENER
 document.body.addEventListener("keydown", function() {
 	document.querySelector("header").style.backgroundColor = "black";
@@ -25,7 +45,7 @@ document.body.addEventListener("keydown", function() {
 	});
 });
 
-//SELECT EVENT LISTENER
+//DBLCLICK EVENT LISTENER
 document.querySelectorAll("h4").forEach(el => {
 	el.addEventListener("dblclick", function() {
 		el.style.backgroundColor = "blue";
